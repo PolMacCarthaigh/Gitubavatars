d995239a3bd039d822646fef4b23c9f56e4b6833
PolMacCarthaigh <paulmccarthy2010@live.ca>
2016-10-13 19:41:01 +0000

    cleaned up

--- avatar_download.js
+++ avatar_download.js
@@ -4,10 +4,10 @@ var dotenv = require('dotenv').config();
 var repoOwner = process.argv[2];
 var repoName  = process.argv[3];
 
-db.connect({
+
   username: process.env.DB_USER,
   password: process.env.DB_PASS
-});
+
 
 function getRepoContributors(repoOwner, repoName, callback) {
   request({
